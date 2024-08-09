6ad0f51ce3cefebbb7a7316e0e136c53d54df7b2
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-08-09 21:52:00 +0700

    update: complete the architecture

--- Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
+++ Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a38a44d29144ff95ce3d0103ce63573e47a5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
