6781b9a4f3d2b39763a02f892a18a4145c4a7a7b
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-08-11 22:18:37 +0700

    update: section 5-28

---
+++ Restaurants.API/Controllers/RestaurantController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/restaurants")]
+public class RestaurantController(IRestaurantsService restaurantsService) : ControllerBase
+{
+    [HttpGet] 
+    public async Task<IActionResult> GetAll()
+    {
+        var restaurants = await restaurantsService.GetAllRestaurants();
+
+        return Ok(restaurants);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var restaurant = await restaurantsService.GetById(id);
+
+        if (restaurant is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(restaurant);
+    }
+}
